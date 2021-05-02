93c902499d40b5ced4854c0d214e9d2f98114db5
Harshit Garg <hdhhtuuh@gmail.com>
2021-05-02 19:41:46 +0530

    Stack using 2 queues added

---
+++ Data Structures/Stacks/11-Stack-using-2Queues.cpp
@@ -0,0 +1,58 @@
+//Harshit
+ 
+#include "bits/stdc++.h"
+using namespace std;
+
+
+class Stack
+{
+public:
+	queue<int>q1;
+	queue<int>q2;
+
+	void push(int x)
+	{
+		q1.push(x);
+	}
+	void pop()
+	{
+		//remove the last added items from q1
+		//move first n-1 element to q2
+	 	//interchange the name of q1 and q2
+		if(q1.empty())
+			return;
+
+		while(q1.size()>1)
+		{
+			int x=q1.front();
+			q1.pop();
+			q2.push(x);
+		}
+		q1.pop(); //to pop the last element from q1 
+		queue<int>q=q1;
+		q1=q2;
+		q2=q;
+	}
+	int top()
+	{
+		return q1.back();
+	}
+	bool empty()
+	{
+		return q1.size()==0;
+	}
+
+};
+int main()
+{
+	Stack s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+
+	while(!s.empty())
+	{
+		cout<<s.top()<<" ";
+		s.pop();
+	}
+}
