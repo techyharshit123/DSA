a72530624f0084f5effb09e56d83dd3cfd320e0c
Harshit Garg <hdhhtuuh@gmail.com>
2021-05-18 02:12:43 +0530

    delete in BST added

---
+++ Data Structures/Binary Search Tree/02-Delete-in-BST.cpp
@@ -0,0 +1,190 @@
+//Harshit
+ 
+#include "bits/stdc++.h"
+using namespace std;
+
+
+class node{
+public:
+    int data;
+    node*left;
+    node*right;
+
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+// Insert in BST
+node* insert(node* root, int key)
+{
+	if(root==NULL)
+	{
+		node* newNode=new node(key);
+		return newNode;
+	}
+
+	if(key<root->data)
+	{
+		root->left=insert(root->left,key);
+	}
+	else
+	{
+		root->right=insert(root->right,key);
+	}
+
+
+	return root;
+}
+
+node* getInorderSuccessor(node* root)
+{
+	node* curr=root->right;
+
+	while(curr!=NULL and curr->left!=NULL)
+	{	
+		curr=curr->left;
+	}
+
+	return curr;
+}
+node* deleteNode(node* root,int key)
+{
+	if(root==NULL)
+		return root;
+
+	if(key<root->data)
+	{
+		root->left=deleteNode(root->left,key);
+	}
+	else if(key>root->data)
+	{
+		root->right=deleteNode(root->right,key);
+	}
+	else
+	{
+		// if node with one children
+		if(root->left==NULL)
+		{
+			node* temp=root->right;
+			delete(root);
+			return temp;
+		}
+		// if node with one children
+		else if(root->right==NULL)
+		{
+			node* temp=root->left;
+			delete(root);
+			return temp;
+		}
+		else
+		{
+			// get the inorder successor of root(smallest in the right subtree)
+			node *temp=getInorderSuccessor(root);
+			root->data=temp->data;
+			// now delete this temp leaf node (inorder successor) as the normal method
+			root->right=deleteNode(root->right,temp->data);
+		}
+	}
+	return root;
+}
+
+
+//////////////////////////////////////////***************METHOD-2*****************////////////////////////////////////////////
+
+/////////To optimise the code we can simply keep an pointer to previous of successor node so that we need not traverse the tree again for deleting the successor node
+
+node* deleteNodeOptimised(node* root,int key)
+{
+	if(root==NULL)
+		return root;
+
+	if(key<root->data)
+	{
+		root->left=deleteNode(root->left,key);
+	}
+	else if(key>root->data)
+	{
+		root->right=deleteNode(root->right,key);
+	}
+	else
+	{
+		// if node with one children
+		if(root->left==NULL)
+		{
+			node* temp=root->right;
+			delete(root);
+			return temp;
+		}
+		// if node with one children
+		else if(root->right==NULL)
+		{
+			node* temp=root->left;
+			delete(root);
+			return temp;
+		}
+		else
+		{
+			node* successorParent=root;
+
+			//find the successor
+			node* successor=root->right;
+
+			while(successor!=NULL and successor->left!=NULL)
+			{	
+				successorParent=successor;
+				successor=successor->left;
+			}
+
+			if(successorParent==root)
+			{
+				successorParent->right=successor->right;
+			}
+			else
+			{
+				successorParent->left=successor->right;
+			}
+			
+			root->data=successor->data;
+			delete(successor);
+			
+		}
+	}
+	return root;
+}
+
+// print BST in inorder fashion
+void printBSTinorder(node * root)
+{
+	if(root==NULL)
+		return;
+
+	//left root right
+	printBSTinorder(root->left);
+	cout<<root->data<<" ";
+	printBSTinorder(root->right);
+}
+int main()
+{
+	node* root=NULL;
+
+	int a[]={8,3,10,1,6,14,4,7,13};
+
+	for(auto x:a)
+	{
+		root=insert(root,x);
+	}
+
+	printBSTinorder(root);
+	cout<<endl;
+
+	root=deleteNodeOptimised(root,1);
+
+	printBSTinorder(root);
+	cout<<endl;
+
+
+}
