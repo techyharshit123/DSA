8ed2902a376b59994a4c52592c3e016fc6e1ec7c
Harshit Garg <hdhhtuuh@gmail.com>
2021-06-01 15:57:14 +0530

    permutation with case change added

--- Algorithms/Recursion/09-Permutation-with-case-change.cpp
+++ Algorithms/Recursion/09-Permutation-with-case-change.cpp
@@ -1,5 +1,5 @@
 //Harshit
-// returned vector should contain (A B C) (A BC) (AB C) (ABC)
+
 #include "bits/stdc++.h"
 using namespace std;
 // *******************************************************************************
@@ -20,7 +20,9 @@ void solve(string input,string output)
     string output2=output;
     
 
-    output1.push_back(input[0]);
+    output1.push_back(input[0]); //pehle output m same case ka letter rakho --> choice1
+
+    //doosre output m doosre case waala choice hoga --> choice2
     if(input[0]>='a' and input[0]<='z')
     output2.push_back(toupper(input[0]));
     else if(input[0]>='A' and input[0]<='Z')
