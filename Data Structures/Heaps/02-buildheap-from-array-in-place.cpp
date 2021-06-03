fb0f0f6cd171e1e6d6ecfd2b6c02d8d849419917
Harshit Garg <hdhhtuuh@gmail.com>
2021-06-04 01:17:31 +0530

    added optimised code

--- Data Structures/Heaps/02-buildheap-from-array-in-place.cpp
+++ Data Structures/Heaps/02-buildheap-from-array-in-place.cpp
@@ -9,8 +9,10 @@ void print(vector<int>v)
 	{
 		cout << x << " ";
 	}
+	cout << endl;
 }
 
+//O(nlogn)
 void buildMaxHeap(vector<int>&v)
 {
 	for (int i = 2; i < v.size(); i++)
@@ -26,9 +28,48 @@ void buildMaxHeap(vector<int>&v)
 		}
 	}
 }
+
+//O(n)
+//Let say we are making a max heap so writing code for maxheapify
+void heapify(int index, vector<int>&v)
+{
+	int left = 2 * index;
+	int right = 2 * index + 1;
+
+	int largest = index;
+	int last = v.size() - 1;
+	if (left <= last and v[left] > v[largest])
+	{
+		largest = left;
+	}
+	if (right <= last and v[right] > v[largest])
+	{
+		largest = right;
+	}
+
+	if (largest != index)
+	{
+		swap(v[largest], v[index]);
+		heapify(largest, v);
+	}
+
+
+}
+void buildMaxHeapOptimised(vector<int>&v)
+{
+	int n = v.size() - 1; //as we have blocked a index that is 0th index so subtracting -1 to compensate
+	for (int i = n / 2; i >= 1; i--)
+	{
+		heapify(i, v);
+	}
+}
 int main()
 {
 	vector<int>v{ -1, 10, 20, 5, 6, 1, 8, 9, 4}; //given heap
 	print(v);
-	buildMaxHeap(v);
+	// buildMaxHeap(v);
+	// print(v);
+	buildMaxHeapOptimised(v);
+	print(v);
+
 }
