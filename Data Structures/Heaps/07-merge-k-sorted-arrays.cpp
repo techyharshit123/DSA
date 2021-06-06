7005adab24ad04c3d4ecca2f03e005d743145fe8
Harshit Garg <hdhhtuuh@gmail.com>
2021-06-06 11:56:04 +0530

    merge k sorted arrays added

--- Data Structures/Heaps/07-merge-k-sorted-arrays.cpp
+++ Data Structures/Heaps/07-merge-k-sorted-arrays.cpp
@@ -1,11 +1,47 @@
-#include "bits/stdc++.h"
+#include<bits/stdc++.h>
 using namespace std;
 
+typedef pair<int, pair<int, int>> node;
+
+vector<int> mergeKArrays(vector<vector<int> > arr) {
+    //Complete this function
+    vector<int> res;
+    priority_queue<node, vector<node>, greater<node>>pq;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        pq.push({arr[i][0], {i, 0}});
+    }
+
+    while (!pq.empty())
+    {
+        node current = pq.top();
+        pq.pop();
+
+        int element = current.first;
+        int xidx = current.second.first;
+        int yidx = current.second.second;
+        res.push_back(element);
+        yidx++;
+
+        if (yidx < arr[xidx].size())
+        {
+            pq.push({arr[xidx][yidx], {xidx, yidx}});
+        }
+
+    }
+
+    return res;
+}
 
 int main()
 {
-	int n, k;
-	cin >> n >> k;
-	vector<vector<int>>v;
+    vector<vector<int>>arr = {{2, 5, 6, 8},
+        {3, 6, 776, 888, 900},
+        {2, 234, 454}
+    };
 
+    vector <int> result = mergeKArrays(arr);
+    for (auto x : result)
+        cout << x << " ";
 }
