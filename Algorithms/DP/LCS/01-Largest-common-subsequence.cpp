6d1de1b9af021ef505521c8a2b043fb8f9310057
Harshit Garg <hdhhtuuh@gmail.com>
2021-06-16 11:52:59 +0530

    longest common subsequence added

--- Algorithms/DP/LCS/01-Largest-common-subsequence.cpp
+++ Algorithms/DP/LCS/01-Largest-common-subsequence.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 ///////////////////// METHOD-1
 // Recursive
-// TC = Exponential
+// TC = Exponential = O(n*(2^n))
+// In order to find out the complexity of brute force approach, we need to first know the number of possible different subsequences of a string with length n, i.e., find the number of subsequences with lengths ranging from 1,2,..n-1. Recall from theory of permutation and combination that number of combinations with 1 element are nC1. Number of combinations with 2 elements are nC2 and so forth and so on. We know that nC0 + nC1 + nC2 + … nCn = 2^n. So a string of length n has 2^n-1 different possible subsequences since we do not consider the subsequence with length 0. This implies that the time complexity of the brute force approach will be O(n * 2^n). Note that it takes O(n) time to check if a subsequence is common to both the strings. This time complexity can be improved using dynamic programming.
 int lcs(int x, int y, string s1, string s2)
 {
     if (x == 0 or y == 0)
