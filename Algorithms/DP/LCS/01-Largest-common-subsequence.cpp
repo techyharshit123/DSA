a575d0a86969300cf459d3ed2264ae9d013414ec
Harshit Garg <hdhhtuuh@gmail.com>
2021-06-16 23:25:10 +0530

    Longest common subsequence and substring added

--- Algorithms/DP/LCS/01-Largest-common-subsequence.cpp
+++ Algorithms/DP/LCS/01-Largest-common-subsequence.cpp
@@ -6,6 +6,7 @@ using namespace std;
 ///////////////////// METHOD-1
 // Recursive
 // TC = Exponential = O(n*(2^n))
+// The naive solution for this problem is to generate all subsequences of both given sequences and find the longest matching subsequence
 // In order to find out the complexity of brute force approach, we need to first know the number of possible different subsequences of a string with length n, i.e., find the number of subsequences with lengths ranging from 1,2,..n-1. Recall from theory of permutation and combination that number of combinations with 1 element are nC1. Number of combinations with 2 elements are nC2 and so forth and so on. We know that nC0 + nC1 + nC2 + … nCn = 2^n. So a string of length n has 2^n-1 different possible subsequences since we do not consider the subsequence with length 0. This implies that the time complexity of the brute force approach will be O(n * 2^n). Note that it takes O(n) time to check if a subsequence is common to both the strings. This time complexity can be improved using dynamic programming.
 int lcs(int x, int y, string s1, string s2)
 {
@@ -100,6 +101,9 @@ int lcsbottomup(int x, int y, string s1, string s2)
     return dp[x][y];
 }
 
+// Method-4
+// for space optimised solution check out the below link
+// https://www.geeksforgeeks.org/space-optimized-solution-lcs/
 int main()
 {
     int t, n, k, x, y;
---
+++ Algorithms/DP/LCS/02-Longest-common-substring.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Method-1
+// Recursion
+// TC = O(n*(m^2))
+
+
+// Method-2
+// TC= o(m*n)
+int longestCommonSubstr (string S1, string S2, int n, int m)
+{
+    int dp[1001][1001];
+    // memset(dp,-1,sizeof(dp));
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            dp[i][j] = -1;
+            if (i == 0 or j == 0)
+                dp[i][j] = 0;
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (S1[i - 1] == S2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = 0;
+            }
+            ans = max(ans, dp[i][j]);
+        }
+    }
+    return ans;
+    // your code here
+}
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        cout << longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
