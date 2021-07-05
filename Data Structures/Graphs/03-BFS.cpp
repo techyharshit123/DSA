b8db2328fdc5b39ca32a82d1074418fa41ec1155
Harshit Garg <hdhhtuuh@gmail.com>
2021-07-05 16:22:11 +0530

    bfs added

--- Data Structures/Graphs/03-BFS.cpp
+++ Data Structures/Graphs/03-BFS.cpp
@@ -67,14 +67,14 @@ public:
 int main()
 {
 	Graph g(5);
-	g.addEdge(1,2);
-	g.addEdge(2,4);
-	g.addEdge(2,3);
-	g.addEdge(1,3);
-	g.addEdge(3,5);
-	g.addEdge(4,5);
-	g.addEdge(3,4);
+	g.addEdge(1, 2);
+	g.addEdge(2, 4);
+	g.addEdge(2, 3);
+	g.addEdge(1, 3);
+	g.addEdge(3, 5);
+	g.addEdge(4, 5);
+	g.addEdge(3, 4);
 	g.printadj();
 
-	g.bfs();
+	g.bfs(1, 5);
 }
